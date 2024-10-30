b9248624823b245610c260a65cdcd8d4d5e47b9d
DANR1BEIRO <danribeiro001@gmail.com>
2024-10-30 17:37:14 -0300

    heroi

---
+++ desafio_nivel_de_heroi.py
@@ -0,0 +1,75 @@
+""" 1 Crie uma variável para armazenar o nome e a quantidade de XP de um herói
+ 2 Utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+ Se XP < 1000: Ferro
+ Se XP > 1000 e < 2000: Bronze
+ Se XP > 2000 e < 5000: Prata
+ Se XP > 5000 e < 7000: Ouro
+ Se XP > 7000 e < 8000: Platina
+ Se XP > 8000 e < 9000: Ascendente
+ Se XP > 9000 e < 10000: Imortal
+ Se XP > 10000 = Radiante
+ 
+ saída:
+ 
+ O herói de nome {nome} está no nível {nivel}
+ """
+class Heroi:
+    def __init__(self, nome, xp, nivel):
+        self.nome = nome
+        self.xp = xp
+        self.nivel = nivel
+
+def heroi_nome():
+    while True:
+        nome = input("Informe o nome do herói: ").strip().title()
+        if nome.isalpha():
+            return nome
+        else:
+            print("Só são permitidas letras para nome.")
+    
+def heroi_xp():
+    while True:
+        try:
+            xp = float(input("Informe a quantidade de XP: "))
+            if xp >= 0:
+                return xp
+            else:
+                print("A quantidade de XP não pode ser menor do que 0!")
+        except ValueError:
+            print("Use apenas números para a XP!")
+            
+def heroi_nivel(xp):
+    if xp < 1000:
+        return "ferro"
+    elif xp < 2000:
+        return "bronze"
+    elif xp < 5000:
+        return "prata"
+    elif xp < 7000:
+        return "ouro"
+    elif xp < 8000:
+        return "platina"
+    elif xp < 9000:
+        return "ascendente"
+    elif xp < 10000:
+        return "imortal"
+    else:
+        return "radiante"
+            
+def main():
+    nome = heroi_nome()
+    xp = heroi_xp()
+    nivel = heroi_nivel(xp)
+    
+    if xp < 0:
+        print("A quantidade de XP não pode ser menor do que zero")
+        return
+    
+    print(f"O herói de nome {nome} tem {xp:.0f} de XP e está no nível {nivel}")
+    
+main()
+     
+
+
+        
--- index.py
+++
@@ -1,2 +0,0 @@
-nome = "Daniel"
-print(f"oi {nome}")
